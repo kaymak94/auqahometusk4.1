24b10e7ad72cf25212069a781091eb137427d26c
Каймакчи Александр <kaymakchiao@mail.ru>
2022-09-20 00:00:19 +0300

    add allure

--- src/test/java/OrderCardWithDeliveryTest.java
+++ src/test/java/OrderCardWithDeliveryTest.java
@@ -1,5 +1,9 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
@@ -13,13 +17,23 @@ import static com.codeborne.selenide.Selenide.*;
 
 public class OrderCardWithDeliveryTest {
 
-    public String generateDate(int days) {
+    public static String generateDate(int days) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
     @Test
@@ -38,5 +52,4 @@ public class OrderCardWithDeliveryTest {
 
 
     }
-
 }
